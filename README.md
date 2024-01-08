013b5354422f53a5576198de34474d4c3fab1161
Artur Lu <artur.lu@yahoo.com>
2024-01-08 09:06:33 +0200

    Starting again

---
+++ README.md
@@ -0,0 +1,75 @@
+Kotobudget: An Accounting Tool
+==============================
+
+Models
+------
+
+### Currency
+Represents a type of currency.
+
+*   **Code:** A short code (max length: 3, unique).
+*   **Name:** The full name of the currency (max length: 255).
+
+
+### Account
+Represents a financial account, such as a bank or cash account.
+
+*   **Name:** A name for the account (max length: 255).
+*   **Description:** Additional information about the account (optional).
+*   **Currency:** The type of currency used in the account.
+*   **Balance:** The current balance of the account (max digits: 20, decimal places: 2, default: 0).
+
+
+### Transaction
+Represents a financial transaction, capturing details of income, expense, or transfer between accounts.
+
+*   **Timestamp:** The date and time of the transaction.
+*   **Account:** The account involved in the transaction.
+*   **Transaction Type:** The type of transaction (Income, Expense, Transfer).
+*   **Amount:** The monetary amount of the transaction (max digits: 10, decimal places: 2, default: 0.00).
+*   **Currency:** The currency used in the transaction.
+*   **Description:** Additional details about the transaction (optional).
+
+
+### ExchangeRate
+Represents exchange rates for different currencies.
+
+*   **Timestamp:** The date and time when the exchange rate is recorded.
+*   **Name:** A name or identifier for the exchange rate (max length: 255).
+*   **Currency:** The currency for which the exchange rate is defined.
+*   **Rate:** The exchange rate value (max digits: 10, decimal places: 4, default: 38.0775).
+
+
+### TaxRule
+Represents a rule for calculating taxes.
+
+*   **Name:** A name or identifier for the tax rule (max length: 255).
+*   **Description:** Additional details about the tax rule.
+*   **Tax Type:** The type of tax calculation (Percentage, Fixed Amount, default: Percentage).
+*   **Percentage:** The percentage value for tax calculation (max digits: 5, decimal places: 2, default: 5.00, optional).
+*   **Fixed Amount:** A fixed amount for tax calculation (max digits: 6, decimal places: 2, default: 1562.00, optional).
+
+
+### Transfer
+Represents a transfer of money between two accounts, possibly in different currencies.
+
+*   **Timestamp:** The date and time of the transfer.
+*   **Source Account:** The account from which the money is transferred.
+*   **Destination Account:** The account to which the money is transferred.
+*   **Amount:** The monetary amount of the transfer (max digits: 10, decimal places: 2, default: 0.00).
+*   **Source Currency:** The currency used in the source account.
+*   **Destination Currency:** The currency used in the destination account.
+*   **Exchange Rate:** The exchange rate applied to the transfer.
+*   **Description:** Additional details about the transfer (optional).
+
+
+### Usage
+#### Install required packages:
+`pip install -r requirements.txt`
+
+#### Apply migrations:
+`python manage.py makemigrations`
+`python manage.py migrate`
+
+#### Run the development server:
+`python manage.py runserver`
--- kotobudget/budget/models.py
+++ kotobudget/budget/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Currency(models.Model):
+    """
+    Represents a currency.
+    """
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+
 class Account(models.Model):
     """
     Represents a financial account, such as a bank account or a cash account.
     """
     name = models.CharField(max_length=255)
-    currency = models.CharField(max_length=3)
+    description = models.TextField(blank=True, null=True)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
     def __str__(self):
@@ -23,11 +35,11 @@ class Transaction(models.Model):
         EXPENSE = 'EX', 'Expense'
         TRANSFER = 'TR', 'Transfer'
 
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='transactions')
     transaction_type = models.CharField(max_length=2, choices=TransactionType.choices)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    currency = models.CharField(max_length=3)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -38,10 +50,10 @@ class ExchangeRate(models.Model):
     """
     Represents exchange rates for different currencies.
     """
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255)
-    currency = models.CharField(max_length=3)
-    rate = models.DecimalField(max_digits=10, decimal_places=4, default=0.0000)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    rate = models.DecimalField(max_digits=10, decimal_places=4, default=38.0775)
 
     def __str__(self):
         return f"{self.name} - {self.rate} {self.currency}"
@@ -58,41 +70,25 @@ class TaxRule(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     tax_type = models.CharField(max_length=3, choices=TaxType.choices, default=TaxType.PERCENTAGE)
-    percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.00, blank=True, null=True)
-    fixed_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2, default=5.00, blank=True, null=True)
+    fixed_amount = models.DecimalField(max_digits=6, decimal_places=2, default=1562.00, blank=True, null=True)
 
     def __str__(self):
         return self.name
-
-
-class TaxPayment(models.Model):
-    """
-    Represents a payment made for taxes based on a specific tax rule.
-    """
-    timestamp = models.DateTimeField()
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='tax_payments')
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    currency = models.CharField(max_length=3)
-    tax_rule = models.ForeignKey(TaxRule, on_delete=models.CASCADE)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return f"Tax Payment - {self.amount} {self.currency}"
 
 
 class Transfer(models.Model):
     """
     Represents a transfer of money between two accounts, possibly in different currencies.
     """
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     source_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='source_transfers')
     destination_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='destination_transfers')
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    source_currency = models.CharField(max_length=3)
-    destination_currency = models.CharField(max_length=3)
-    exchange_rate = models.DecimalField(max_digits=10, decimal_places=4, default=1.0000)
+    source_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='source_transfers')
+    destination_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='destination_transfers')
+    exchange_rate = models.ForeignKey(ExchangeRate, on_delete=models.CASCADE, related_name='transfers')
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"Transfer - {self.amount} {self.source_currency} to {self.destination_currency}"
-
